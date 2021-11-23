b3cf3680d69e312f7c44522c6cf386bf4c58c333
cchowthi <christal.chowthi@gilead.com>
2021-11-23 10:16:51 -0800

    added stuff

---
+++ some_script.py
@@ -0,0 +1,10 @@
+#!/biometrics/users/cchowthi/public/qualvenv/bin/python3
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#Program:		latestDate function
+#Coded on: 		12JAN2021 by AJ Johansson 
+#Description:	sorts by date and removes the newest from the list
+#							
+##############################################################################
+
